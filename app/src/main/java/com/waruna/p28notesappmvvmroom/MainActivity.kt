ebbff9a0d30402901e9af1a0c8474f5e241c69b7
warunagit <waruna.git@gmail.com>
2025-02-28 12:05:21 +0200

    fragments home, new, update created

--- app/src/main/java/com/waruna/p28notesappmvvmroom/MainActivity.kt
+++ app/src/main/java/com/waruna/p28notesappmvvmroom/MainActivity.kt
@@ -7,6 +7,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // NotesApp contains all of:
+    // LiveView,DataBinding,MVVM,ROOM,Navigation
+
+    //1-add dependencies
+    //2-add NavHostFragment(FragmentContainerView)->
+    // NavGraph to activity_main.xml
+    //3-add destinations to nav_graph.xml
+    //4-change FrameLayouts to ConstraintLayouts
+    //5-add FloatingActionButton to fragment_home
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
