a215f93544cbfddf2d5e6d56c4f3916cce16c23c
warunagit <waruna.git@gmail.com>
2025-03-05 11:24:22 +0200

    done 237

--- app/src/main/java/com/waruna/p28notesappmvvmroom/fragments/UpdateNoteFragment.kt
+++ app/src/main/java/com/waruna/p28notesappmvvmroom/fragments/UpdateNoteFragment.kt
@@ -2,17 +2,41 @@ package com.waruna.p28notesappmvvmroom.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.waruna.p28notesappmvvmroom.MainActivity
 import com.waruna.p28notesappmvvmroom.R
+import com.waruna.p28notesappmvvmroom.adapter.NoteAdapter
+import com.waruna.p28notesappmvvmroom.databinding.FragmentHomeBinding
+import com.waruna.p28notesappmvvmroom.databinding.FragmentNewNoteBinding
+import com.waruna.p28notesappmvvmroom.databinding.FragmentUpdateNoteBinding
+import com.waruna.p28notesappmvvmroom.model.Note
+import com.waruna.p28notesappmvvmroom.viewmodel.NoteViewModel
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UpdateNoteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class UpdateNoteFragment : Fragment() {
+class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
+
+    private var _binding: FragmentUpdateNoteBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var notesViewModel: NoteViewModel
+
+    private lateinit var currentNote: Note
+
+
+    //since nav_graph receives arguments
+    private val args: UpdateNoteFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
 
     override fun onCreateView(
@@ -20,7 +44,69 @@ class UpdateNoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_note, container, false)
+        _binding = FragmentUpdateNoteBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        notesViewModel = (activity as MainActivity).noteViewModel
+        currentNote = args.note!!
+        binding.etUpdateNoteTitleUpdate.setText(currentNote.noteTitle)
+        binding.etUpdateNoteBody.setText(currentNote.noteBody)
+
+        //if the user update the note
+        binding.btnUpdateDone.setOnClickListener {
+            val title = binding.etUpdateNoteTitleUpdate.text.toString().trim()
+            val body = binding.etUpdateNoteBody.text.toString().trim()
+
+            if (title.isNotEmpty()) {
+                val note = Note(currentNote.id, title, body)
+                notesViewModel.updateNote(note)
+                view.findNavController().navigate(R.id.action_updateNoteFragment_to_homeFragment)
+            } else {
+                Toast.makeText(context, "Please enter note title", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun deleteNote() {
+        AlertDialog.Builder(requireContext()).apply {
+            setTitle("Delete Note")
+            setMessage("Are you sure to delete?")
+            setPositiveButton("Delete") { _,_ ->
+                notesViewModel.deleteNote(currentNote)
+
+                view?.findNavController()?.navigate(
+                    R.id.action_updateNoteFragment_to_homeFragment
+                )
+            }
+
+            setNegativeButton("Cancel", null)
+        }.create().show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_update_note,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_delete -> {
+                deleteNote()
+            }
+        }
+        return  super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
